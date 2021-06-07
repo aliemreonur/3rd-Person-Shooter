316f0bb44d6cb6df9d5e134be904f9ab69a12624
aliemreonur <58938983+aliemreonur@users.noreply.github.com>
2021-06-07 18:52:03 +0300

    Reload Anim Completed

--- Assets/GameDevHQ/FileBase/Extensions/Systems/Controls/FPS_Character_Controller/Scripts/FPS_Controller.cs
+++ Assets/GameDevHQ/FileBase/Extensions/Systems/Controls/FPS_Character_Controller/Scripts/FPS_Controller.cs
@@ -21,6 +21,8 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
 
         private CharacterController _controller; //reference variable to the character controller component
         private float _yVelocity = 0.0f; //cache our y velocity
+
+        private bool _headBob;
         
 
         [Header("Headbob Settings")]       
@@ -59,7 +61,7 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
             }
             else if (Input.GetKeyDown(KeyCode.Alpha2))
             {
-                _controller.height = 1.0f;
+               // _controller.height = 1.0f;
             }
         }
 
@@ -126,6 +128,11 @@ namespace GameDevHQ.FileBase.Plugins.FPS_Character_Controller
 
         void HeadBobbing()
         {
+            if(_headBob == false)
+            {
+                return;
+            }
+
             float h = Input.GetAxis("Horizontal"); //horizontal inputs (a, d, leftarrow, rightarrow)
             float v = Input.GetAxis("Vertical"); //veritical inputs (w, s, uparrow, downarrow)
 
